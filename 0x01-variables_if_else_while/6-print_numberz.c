549f92a8a0a576e8692f390cb2d14a06a93be443
Belovetech <belovetech@gmail.com>
2022-03-11 21:30:42 +0000

    Added numberz file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Print all numbers in base 10
+*
+* Return: Always (0) success
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar(i);
+	putchar('\n');
+
+	return (0);
+}
