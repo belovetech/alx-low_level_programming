c1adfa894db12b604f77fa46e796039b7f30c4d5
Belovetech <belovetech@gmail.com>
2022-03-11 23:25:07 +0000

    Added 100-combo

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,14 +13,17 @@ int main(void)
 	for (d = '0'; d <= '9'; d++)
 	{
 		putchar(d);
+	}
 	for (i = d; i <= d; i++)
+	{
 		putchar(i);
+	}
 		if (d != '9')
 		{
 			putchar(',');
 			putchar(' ');
 		}
-	}
+
 	putchar('\n');
 
 	return (0);
