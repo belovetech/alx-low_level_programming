6906d903d941fb78f97bbdb7b667423de72a3778
Belovetech <belovetech@gmail.com>
2022-03-11 20:29:22 +0000

    Added print-number file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Print all single numbers of base 10
+ *
+ * Return: Always (0) success
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	while (i < 10)
+		printf("%d", i);
+		i++;
+	printf("\n");
+
+	return (0);
+}
