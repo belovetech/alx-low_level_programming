8ffb7e2bf9d50512e3566b1c8d4f9b7e0d20b00e
Belovetech <belovetech@gmail.com>
2022-03-11 22:15:48 +0000

    Added hexadecimal file

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,13 +10,9 @@ int main(void)
 	int i;
 
 	for (i = 0; i < 10; i++)
-	{
 		putchar((char)i % 10 + '0');
-	}
-	for (i = 'a'; i < 'g'; i++);
-	{
+	for (i = 'a'; i < 'g'; i++)
 		putchar(i);
-	}
 	putchar('\n');
 
 	return (0);
