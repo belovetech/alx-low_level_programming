fb5b029fc0d197a31eb9de086cea45eeb06d4046
Belovetech <belovetech@gmail.com>
2022-03-15 15:43:13 +0000

    Added 1-alphabet file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,5 @@
 #include "main.h"
 
-/* print_alphabet prototype */
-void print_alphabet(void);
-
-/**
-* main - check the code
-*
-* Return: Always 0 (success).
-*/
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
 /**
 * print_alphabet - writes alphabet from a-z
 * @c -  character
