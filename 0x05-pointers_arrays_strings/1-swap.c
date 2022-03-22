021dbf17239f914b791ec078c4faef5ca13ba71e
belovetech <belovetech@gmail.com>
2022-03-22 01:19:47 +0000

    Added a file

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* swap_int - swap the value of two integers
+* @a: integer one
+* @b: integer two
+*
+* Return: void
+*/
+
+void swap_int(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
