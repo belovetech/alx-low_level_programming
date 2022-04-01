aa9af982cdacf8a2129ce1de49ecdec1e63345e4
belovetech <belovetech@gmail.com>
2022-04-01 19:01:31 +0000

    added 3-mul files

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - multiply two numbers
+* @argc: arguement count
+* @argv: arguement vectors
+*
+* Return: Always 0 (succes)
+*/
+
+int main(int argc, char *argv[])
+{
+	int i, sum = 1;
+
+	if (argc > 2)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			sum *= atoi(argv[i]);
+		}
+		printf("%d\n", sum);
+		return (sum);
+	}
+	else
+	{
+
+		printf("Error\n");
+		return (1);
+	}
+
+	exit(EXIT_SUCCESS);
+}
