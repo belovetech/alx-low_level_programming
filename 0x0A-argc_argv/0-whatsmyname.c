0ca3c776745a941c883f48cc651c86f3993e985e
belovetech <belovetech@gmail.com>
2022-04-01 17:52:47 +0000

    Added 0-whatsmyname file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - print the name of the program
+* @argc: arguement count
+* @argv: arguement vectors
+*
+* Return: Always 0 (succes)
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	
+	(void)argc;
+
+	exit(EXIT_SUCCESS);
+}
