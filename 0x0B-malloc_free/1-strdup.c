833dbd60d710a10b7412206f635f8b2879606931
belovetech <belovetech@gmail.com>
2022-04-05 20:17:14 +0000

    duplictaes of the strings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* _strdup - duplictates string
+* @str: string
+*
+* Return: pointer to the new string
+*/
+
+char *_strdup(char *str)
+{
+	char *ptr;
+	int i = 0;
+	int len = 1;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len] != '\0')
+		len++;
+
+	ptr = (char *)malloc(len * sizeof(char));
+
+	if (ptr == NULL)
+		return (NULL);
+
+	while (str[i] != '\0')
+	{
+		ptr[i] = str[i];
+		i++;
+	}
+	return (ptr);
+	free(ptr);
+}
+
+
