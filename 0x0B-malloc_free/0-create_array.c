a0a11aede67db6bda087c638c9b5250508ef3692
belovetech <belovetech@gmail.com>
2022-04-05 19:03:53 +0000

    creates an array of character

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -20,11 +20,15 @@ char *create_array(unsigned int size, char c)
 
 	ptr = (char *)malloc(size * sizeof(char));
 
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
 	for (i = 0; i < size; i++)
 	{
 		ptr[i] = c;
 	}
-
 	return (ptr);
 	free(ptr);
 }
