3205ca44b9ac24a2c4ff565887f7a82e66656b51
belovetech <belovetech@gmail.com>
2022-04-06 01:59:29 +0000

    Added str concat file

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -34,26 +34,34 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 		s1 = "";
-	else if (s2 == NULL)
+	if (s2 == NULL)
 		s2 = "";
 
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
 	size = len1 + len2;
 
-	ptr = (char *)malloc((size + 1) * sizeof(char));
-
-	if (ptr == NULL)
-		return (NULL);
-
-	for (i = 0, j = 0; j < (size + 1); i++)
+	if (s1 != NULL && s2 != NULL)
 	{
-		if (i < len1)
-			ptr[i] = s1[i];
-		else
-			ptr[i] = s2[j++];
+		ptr = (char *)malloc((size + 1) * sizeof(char));
+
+		if (ptr == NULL)
+			return (NULL);
+
+		for (i = 0, j = 0; j < (size + 1); i++)
+		{
+			if (i < len1)
+				ptr[i] = s1[i];
+			else
+				ptr[i] = s2[j++];
+		}
+		return (ptr);
 	}
-	return (ptr);
+	else
+	{
+		ptr = "";
+	}
+
 	free(ptr);
 }
 
