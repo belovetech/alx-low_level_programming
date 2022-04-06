3a840bd4b9d7f6d01bc0c3d44b573366d7496676
belovetech <belovetech@gmail.com>
2022-04-06 01:34:24 +0000

    Added str concat file

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -32,8 +32,10 @@ char *str_concat(char *s1, char *s2)
 	char *ptr;
 	int len1, len2, size, i, j;
 
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
