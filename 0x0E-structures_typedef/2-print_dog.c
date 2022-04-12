f6f38d32a5665bf220ddb444a020935574d8db79
belovetech <belovetech@gmail.com>
2022-04-12 06:06:44 +0000

    check for the age element

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,7 +14,10 @@ void print_dog(struct dog *d)
 			printf("Name: %s\n", d->name);
 		else
 			printf("Name: (nil)\n");
-		printf("Age: %f\n", d->age);
+		if (d->age != '\0')
+			printf("Age: %f\n", d->age);
+		else
+			printf("Name: (nil)\n");
 		if (d->owner != NULL)
 			printf("Owner: %s\n", d->owner);
 		else
