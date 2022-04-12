75492b949919033ad5b56047d1c2cc7d0a27cc4a
belovetech <belovetech@gmail.com>
2022-04-12 12:30:20 +0000

    breaking correction into component

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,48 +16,68 @@ int _strlen(char *str)
 }
 
 /**
+ * _strcpy - copy a string
+ * @dest: destination value
+ * @src: source value
+ *
+ * Return: the pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
+
+/**
  * new_dog - creates a new dog
  * @name: name of the dog
  * @age: age of dog
  * @owner: owner of dog
- * * Return: newdog created
+ * Return: newdog created
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *newdog;
-	unsigned int i, j, namelen, ownerlen;
+	int nameLen, ownerLen;
 
-	if (name == NULL || owner == NULL)
-		return (NULL);
-
-	newdog = malloc(sizeof(dog_t));
-	if (newdog == NULL)
-		return (NULL);
-
-	namelen = _strlen(name);
-	newdog->name = malloc((namelen + 1) * sizeof(char));
-	if (newdog->name == NULL)
+	if (name != NULL && owner != NULL)
 	{
-		free(newdog);
-		return (NULL);
+		nameLen = _strlen(name) + 1;
+		ownerLen = _strlen(owner) + 1;
+		newdog = malloc(sizeof(dog_t));
+
+		if (newdog == NULL)
+			return (NULL);
+
+		newdog->name = malloc(sizeof(char) * nameLen);
+
+		if (newdog->name == NULL)
+		{
+			free(newdog);
+			return (NULL);
+		}
+
+		newdog->owner = malloc(sizeof(char) * ownerLen);
+
+		if (newdog->owner == NULL)
+		{
+			free(newdog->name);
+			free(newdog);
+			return (NULL);
+		}
+
+		newdog->name = _strcpy(newdog->name, name);
+		newdog->owner = _strcpy(newdog->owner, owner);
+		newdog->age = age;
 	}
-
-	for (i = 0; i <= namelen; i++)
-		newdog->name[i] = name[i];
-
-	newdog->age = age;
-
-	ownerlen = _strlen(owner);
-	newdog->owner = malloc((ownerlen + 1) * sizeof(char));
-	if (newdog->owner == NULL)
-	{
-		free(newdog->name);
-		free(newdog);
-		return (NULL);
-	}
-
-	for (j = 0; i <= ownerlen; j++)
-		newdog->owner[i] = owner[j];
 
 	return (newdog);
 }
