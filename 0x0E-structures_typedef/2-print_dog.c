18100eb9f062343cfe901f39884475350447d0d9
belovetech <belovetech@gmail.com>
2022-04-12 15:40:10 +0000

    error fixed

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,18 +10,9 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		if (d->name != NULL)
-			printf("Name: %s\n", d->name);
-		else
-			printf("Name: (nil)\n");
-		if (d->age != '\0')
-			printf("Age: %f\n", d->age);
-		else
-			printf("Name: (nil)\n");
-		if (d->owner != NULL)
-			printf("Owner: %s\n", d->owner);
-		else
-			printf("Name: (nil)\n");
+		printf("Name: %s\n", (d->name)? d->name : "(nil)");
+		printf("Name: %f\n", (d->age)? d->age : 0);
+		printf("Name: %s\n", (d->owner)? d->owner : "(nil)");
 	}
 
 }
