d3c0273fdecab90dbe3998b122c0d3732418cd7f
belovetech <belovetech@gmail.com>
2022-04-12 06:14:23 +0000

    Using typdef to create alias

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,14 +7,12 @@
  * @age: second member
  * @owner: third member
  */
-
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
-
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif /* Dog */
