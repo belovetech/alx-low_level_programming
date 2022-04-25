dfe89041cb18adda19b84f86f8b9a49dc5170a5f
belovetech <belovetech@gmail.com>
2022-04-25 21:00:34 +0100

    removed triling whitespace

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -4,7 +4,7 @@
  * add_nodeint_end - adds a new node at the end of a listint_t list.
  * @head: reference to the head pointer
  * @n: new data
- * Result: address of the new element
+ * Return: address of the new element
  */
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
@@ -22,7 +22,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	{
 		last_node = last_node->next;
 	}
-	
+
 	if (new_node != NULL)
 	{
 		new_node->n = n;
