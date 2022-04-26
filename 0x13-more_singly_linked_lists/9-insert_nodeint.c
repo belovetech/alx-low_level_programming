6922d281e6466bdad22e9ef6984ce9369bb5801d
belovetech <belovetech@gmail.com>
2022-04-26 22:38:29 +0100

    Checked if index is 0

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,10 +11,9 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int count = 0;
-	listint_t *ptmp_node = NULL;
-	listint_t *new_node = NULL;
+	listint_t *new_node = NULL, *ptmp_node = NULL;
 
-	if (head == NULL || *head == NULL)
+	if (head == NULL)
 		return (NULL);
 
 	if (idx)
@@ -24,27 +23,28 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			ptmp_node = ptmp_node->next;
 		}
-	}
-
-	if (ptmp_node == NULL)
-	{
-		return (NULL);
-	}
-	else
-	{
-		new_node = malloc(sizeof(listint_t));
-
-		if (new_node != NULL)
-		{
-			new_node->n = n;
-
-			new_node->next = ptmp_node->next;
-
-			ptmp_node->next = new_node;
-		}
-		else
+		if (ptmp_node == NULL)
 			return (NULL);
 	}
+
+	new_node = malloc(sizeof(listint_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+
+	new_node->next = ptmp_node->next;
+
+	ptmp_node->next = new_node;
+
 	return (new_node);
 }
 
