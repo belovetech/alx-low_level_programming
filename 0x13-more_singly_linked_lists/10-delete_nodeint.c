bcccf0577283069a5a57f24db867f13b43c2a827
belovetech <belovetech@gmail.com>
2022-04-26 23:25:30 +0100

    Corrected misspelt variable name

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -32,7 +32,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 
 	next_node = cur_node->next;
-	cur_node->next = next->next;
+	cur_node->next = next_node->next;
 	free(next_node);
 	return (1);
 }
