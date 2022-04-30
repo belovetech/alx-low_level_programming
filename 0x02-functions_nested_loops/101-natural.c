a15d31b04f848f33c88fc665478a22fa07782151
belovetech <belovetech@gmail.com>
2022-04-30 16:16:17 +0100

    Fix bug

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -15,13 +15,14 @@ int main(void)
 	for (i = 0; i < n; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
-			continue;
+			sum += i;
 		else if (i % 3 == 0)
 			sum += i;
 		else if (i % 5 == 0)
 			sum += i;
 	}
 
-	printf("%d \n", sum);
+	printf("\n%d \n", sum);
 	return (0);
 }
+
