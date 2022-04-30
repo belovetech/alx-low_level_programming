63263a462406c16b362dde95c65d2408f8dceddb
belovetech <belovetech@gmail.com>
2022-04-30 14:25:11 +0100

    checked if n equal to zero

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,15 +1,13 @@
 #include "main.h"
-
 /**
  * print_times_table - prints the n times table, starting with 0.
  * @n: number of times table
  */
-
 void print_times_table(int n)
 {
 	int x, y, product = 0;
 
-	if (n <= 15 && n > 0)
+	if (n <= 15 && n >= 0)
 	{
 		for (x = 0; x <= n; x++)
 		{
@@ -21,17 +19,13 @@ void print_times_table(int n)
 					_putchar(',');
 					_putchar(' ');
 				}
-				if (y == 0)
-				{
-					_putchar('0');
-				}
 				if (product >= 100 && (y != 0))
 				{
 					_putchar(product / 100 + '0');
 					_putchar((product / 10) % 10 + '0');
 					_putchar(product % 10 + '0');
 				}
-				else if (product >= 10)
+				else if (product >= 10 && product < 100)
 				{
 					_putchar(' ');
 					_putchar(product / 10 + '0');
@@ -43,6 +37,8 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar(product + '0');
 				}
+				else
+					_putchar(product % 10 + '0');
 			}
 			_putchar('\n');
 		}
