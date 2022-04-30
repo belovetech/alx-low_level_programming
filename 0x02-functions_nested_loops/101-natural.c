83b7bab2c3bfb47f11135a63dac80a55004b69c8
belovetech <belovetech@gmail.com>
2022-04-30 16:48:49 +0100

    Added another function

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * main - Entry point
+ * main - prints the sum of all the multiples of 3 or 5 below 1024
  *
  * Return: 0 always (success)
  */
@@ -22,5 +22,23 @@ int main(void)
 
 	printf("%d\n", sum);
 	return (0);
+}
+
+/**
+ * sum_mul_of_3_and_5 - prints sum of multiplr of 3 and 5
+ * @num: below the number
+ * Return: void
+ */
+
+void sum_mul_of_3_and_5(int num)
+{
+	int i, sum = 0;
+	
+	for (i = 0; i < num; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			sum  += i;
+	}
+	printf("%d\n", sum);
 }
 
