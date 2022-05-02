a852544c76e31dd3b99d7e0a5b560f93048d5605
belovetech <belovetech@gmail.com>
2022-05-02 22:42:49 +0100

    fix bug

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,20 +9,21 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd_to_create, fd_to_write;
+	int fd_to_create, fd_to_write, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd_to_create = open(filename, O_CREAT | O_RDWR, 0600);
-	fd_to_write = write(STDOUT_FILENO, text_content, strlen(text_content));
+	while (text_content[len] != NULL)
+		len++;
 
-	if (fd_to_create != -1 || fd_to_write > 0)
-		return (1);
-	else
+	fd_to_create = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	fd_to_write = write(STDOUT_FILENO, text_content, len);
+
+	if (fd_to_create == -1 || fd_to_write == -1)
 		return (-1);
 
 	close(fd_to_create);
 
-	return (fd_to_write);
+	return (1);
 }
