e4a14e974851ded946fdd42e099a135436aeb92e
belovetech <belovetech@gmail.com>
2022-05-16 23:53:44 +0100

    reverse string using pointer

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -24,3 +24,35 @@ void rev_string(char *s)
 		s[len] = rev_str;
 	}
 }
+
+/**
+ * rev_str  - Reverse string using pointer
+ * @str: string to reverse
+ */
+
+void rev_str(char *str)
+{
+	int i, len = 0;
+	char tmp, *begin_ptr, *end_ptr;
+
+	while (*(str + len))
+		len++;
+
+	begin_ptr = str;
+	end_ptr = str;
+
+	for (i = 0; i < len - 1; i++)
+		end_ptr++;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		tmp = *end_ptr;
+		*end_ptr = *begin_ptr;
+		*begin_ptr = tmp;
+
+		begin_ptr++;
+		end_ptr--;
+	}
+}
+
+
