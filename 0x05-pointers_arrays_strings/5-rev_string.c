613c1e2bb25e904ca9d2a5ccaffd301e7e9c86ca
belovetech <belovetech@gmail.com>
2022-05-16 23:59:01 +0100

    add comment to rev_str

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -35,21 +35,26 @@ void rev_str(char *str)
 	int i, len = 0;
 	char tmp, *begin_ptr, *end_ptr;
 
+	/* Length of the string */
 	while (*(str + len))
 		len++;
 
+	/* Initialize begin_ptr & and end_ptr to point to basse address*/
 	begin_ptr = str;
 	end_ptr = str;
 
+	/* Move end_ptr to the last address of the element */
 	for (i = 0; i < len - 1; i++)
 		end_ptr++;
 
 	for (i = 0; i < len / 2; i++)
 	{
+		/* Swap character */
 		tmp = *end_ptr;
 		*end_ptr = *begin_ptr;
 		*begin_ptr = tmp;
-
+	
+		/* update pointer */
 		begin_ptr++;
 		end_ptr--;
 	}
