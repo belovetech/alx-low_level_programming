b4b03b55c2f501421e107ff5c81bdefe50c3d92b
belovetech <belovetech@gmail.com>
2022-07-02 01:04:00 +0530

    Fix betty error

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -8,25 +8,25 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-    unsigned long int i;
-    hash_table_t *hash_table = ht;
-    hash_node_t *hash_node, *temp;
+	unsigned long int i;
+	hash_table_t *hash_table = ht;
+	hash_node_t *hash_node, *temp;
 
-    for (i = 0; i < ht->size; i++)
-    {
-        if (ht->array[i] != NULL)
-        {
-            hash_node = ht->array[i];
-            while (hash_node != NULL)
-            {
-                temp = hash_node->next;
-                free(hash_node->key);
-                free(hash_node->value);
-                free(hash_node);
-                hash_node = temp;
-            }
-        }
-    }
-    free(hash_table->array);
-    free(hash_table);
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			hash_node = ht->array[i];
+			while (hash_node != NULL)
+			{
+				temp = hash_node->next;
+				free(hash_node->key);
+				free(hash_node->value);
+				free(hash_node);
+				hash_node = temp;
+			}
+		}
+	}
+	free(hash_table->array);
+	free(hash_table);
 }
