55524ff9b87fc5e4d7aad6387862e37ca1f9951b
belovetech <belovetech@gmail.com>
2022-07-02 00:54:51 +0530

    Fix betty error

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -9,31 +9,31 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-    unsigned long int i;
-    hash_node_t *hash_node;
-    unsigned char flag = 0;
+	unsigned long int i;
+	hash_node_t *hash_node;
+	unsigned char flag = 0;
 
-    if (ht == NULL)
-        return;
+	if (ht == NULL)
+		return;
 
-    printf("{");
-    for (i = 0; i < ht->size; i++)
-    {
-        if (ht->array[i] != NULL)
-        {
-            if (flag == 1)
-                printf(", ");
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			if (flag == 1)
+				printf(", ");
 
-            hash_node = ht->array[i];
-            while (hash_node != NULL)
-            {
-                printf("'%s': '%s'", hash_node->key, hash_node->value);
-                hash_node = hash_node->next;
-                if (hash_node != NULL)
-                    printf(", ");
-            }
-            flag = 1;
-        }
-    }
-    printf("}\n");
+			hash_node = ht->array[i];
+			while (hash_node != NULL)
+			{
+				printf("'%s': '%s'", hash_node->key, hash_node->value);
+				hash_node = hash_node->next;
+				if (hash_node != NULL)
+					printf(", ");
+			}
+			flag = 1;
+		}
+	}
+	printf("}\n");
 }
